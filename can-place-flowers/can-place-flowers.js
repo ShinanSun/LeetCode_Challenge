6843caa10281a2f8ab09d34d20976b6f3c4368de
ShinanSun <shinansun0516@gmail.com>
2021-03-29 19:44:39 -0500

    can flace flower

--- can-place-flowers/can-place-flowers.js
+++ can-place-flowers/can-place-flowers.js
@@ -3,27 +3,26 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
-    if (flowerbed.length === 1 && flowerbed[0] === 0) {
-        return n <= 1;
+var canPlaceFlowers = function (flowerbed, n) {
+  if (flowerbed.length === 1 && flowerbed[0] === 0) {
+    return n <= 1;
+  }
+  let count = 0;
+  for (var i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0) {
+      if (flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
+        flowerbed[i] = 1;
+        count++;
+      }
+      if (i === 0 && flowerbed[1] === 0) {
+        flowerbed[i] = 1;
+        count++;
+      }
+      if (i === flowerbed.length - 1 && flowerbed[i - 1] === 0) {
+        flowerbed[i] = 1;
+        count++;
+      }
     }
-    var count = 0;
-    for (let i = 0; i < flowerbed.length; i++) {
-        if (i === 0 && flowerbed[0] === 0 && flowerbed[1] === 0){
-            flowerbed[0] = 1;
-            count ++
-        } else if (i === flowerbed.length -1 && flowerbed[i] === 0 && flowerbed[i-1] === 0) {
-        
-            count ++
-        } else if (flowerbed[i] === 0 && flowerbed[i-1] === 0 && flowerbed[i+1] === 0 ) {
-            flowerbed[i] = 1;
-            count ++
-        }
-    }
-    
-    if (count >= n) {
-        return true
-    } else {
-        return false
-    }
+  }
+  return count >= n;
 };
