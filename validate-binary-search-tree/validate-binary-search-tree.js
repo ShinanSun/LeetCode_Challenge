86bcb03fcfab9c62c54c68976759ae3b47dec0fb
ShinanSun <shinansun0516@gmail.com>
2021-04-22 12:48:54 -0500

    BST

--- validate-binary-search-tree/validate-binary-search-tree.js
+++ validate-binary-search-tree/validate-binary-search-tree.js
@@ -10,22 +10,43 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-    function DFS (node, low, high) {
-        if (!node) return true;
-        if (node.val > low && node.val < high) {
-           
-                var left = DFS (node.left, low, node.val)
-            
-            
-                var right = DFS (node.right, node.val, high)
-        
-            return left && right;
-        } else {
-            return false;
-        }
+var isValidBST = function (root) {
+  function DFS(node, low, high) {
+    if (!node) return true;
+    if (node.val > low && node.val < high) {
+      var left = DFS(node.left, low, node.val);
+
+      var right = DFS(node.right, node.val, high);
+
+      return left && right;
+    } else {
+      return false;
     }
-    
-    return DFS (root, -Infinity, +Infinity)
-  
+  }
+
+  return DFS(root, -Infinity, +Infinity);
+};
+
+// iterative traversal approach
+let isValidBST2 = (root) => {
+  let stack = [],
+    curr = root,
+    prev = null;
+
+  while (stack.length || curr) {
+    if (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    } else {
+      let node = stack.pop();
+      if (prev && node.val <= prev.val) {
+        // prev are all left node of curr node.
+        return false;
+      }
+      prev = node;
+      curr = p.right;
+    }
+  }
+
+  return true;
 };
