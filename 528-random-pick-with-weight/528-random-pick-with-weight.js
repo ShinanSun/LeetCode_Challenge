b75a99a0e6af25f40956291fde3eda82f13022d0
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-01-23 13:03:06 -0600

    Time: 247 ms (54.52%), Space: 47.8 MB (76.03%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.js
+++ 528-random-pick-with-weight/528-random-pick-with-weight.js
@@ -1,33 +1,39 @@
 /**
  * @param {number[]} w
- */[0, 1, 1, 1]
-
+ w = [1, 2, 4];
+ prefixSum = [1, 3, 7]; target = 3
+ index 0 : < 1
+ index 1:   < 3
+ index 2: < 7;
+ // using binarysearch to find the first element that bigger than target;
+ 
+ [0, 1, 1, 3,3,3,3]
+ */
 var Solution = function(w) {
-   this.nums = [w[0]];  //[1,2,3,4,3]
-    for (let i = 1; i < w.length; i++) {
-        this.nums[i] = w[i] + this.nums[i - 1];
+    let length = w.length;
+    this.prefixSums = [w[0]];
+    for (let i = 1; i < length; i++) {
+        this.prefixSums[i] = w[i] + this.prefixSums[i - 1];
     }
-   
 };
 
 /**
  * @return {number}
  */
 Solution.prototype.pickIndex = function() {
-    let length = this.nums.length;
-    let random = Math.floor(Math.random() * this.nums[length - 1]);  //5
+    let length = this.prefixSums.length;
+    let target = Math.floor(Math.random() * this.prefixSums[length - 1]);
     
     let left = 0, right = length - 1;
-    while(left <= right) {
+    while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-        if (random < this.nums[mid]) { //   3 > 5
-            right = mid - 1;
-        } else {
+        if (target >= this.prefixSums[mid]) {
             left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    
-    return left;
+     return left;
 };
 
 /** 
