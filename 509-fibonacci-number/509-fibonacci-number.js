97568a9967b404d2b96e0e384880df35115c3932
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-01-24 22:49:38 -0600

    Time: 110 ms (32.59%), Space: 38.1 MB (91.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,10 +3,15 @@
  * @return {number}
  0, 1, 1, 2, 3, 5, 8
  */
+const cache = new Map();
 var fib = function(n) {
+    if (cache.has(n)) return cache.get(n);
+    
     if (n < 2) {
         return n;
     }
     
-    return fib(n - 1) + fib(n - 2);
+    let currSum = fib(n - 1) + fib(n - 2);
+    cache.set(n, currSum)
+    return currSum;
 };
