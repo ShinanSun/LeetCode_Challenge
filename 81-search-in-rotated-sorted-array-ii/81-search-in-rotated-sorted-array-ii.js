d3f2dd3892b2a8f57ba864a948634af7803bd690
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-01-27 22:20:43 -0600

    Time: 48 ms (100.00%), Space: 42.3 MB (6.11%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.js
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.js
@@ -3,36 +3,32 @@
  * @param {number} target
  * @return {boolean}
  */
-const skipCurrent = function(nums, start, mid) {
-    return nums[start] === nums[mid];
-}
-const existsInFirst = function(startVal, target) {
-    return startVal <= target
-}
 
 var search = function(nums, target) {
     if (!nums.length) return false;
     
-    let start = 0, end = nums.length - 1;
-    while(start <= end) {
-        let mid = Math.floor((start + end) / 2);
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        //to avoid duplicates
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        
+        let mid = Math.floor((left + right) / 2);
         if (nums[mid] === target) return true;
-        if (skipCurrent(nums, start, mid)) {
-            start++;
-            continue;
-        }
-        
-        //which array pivot belong to;
-        let povitInFirst = existsInFirst(nums[start], nums[mid]);
-        let targetInFirst = existsInFirst(nums[start], target);
-        
-        if (povitInFirst && targetInFirst || !povitInFirst && !targetInFirst) {
-            if (nums[mid] < target) start = mid + 1;
-            else end = mid - 1;
+        if (nums[mid] >= nums[left]) {
+            if (target < nums[mid] && target >= nums[left]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         } else {
-            if (povitInFirst) start = mid + 1;
-            else end = mid - 1;
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
+        
     }
     
     return false;
