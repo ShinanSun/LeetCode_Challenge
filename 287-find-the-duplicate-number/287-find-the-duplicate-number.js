4ccdc7246a1e5b0f1f3a79dc8557a95f5a032c79
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-01-29 14:01:10 -0600

    Time: 920 ms (5.00%), Space: 64 MB (5.01%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -2,14 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
+//当前这个数array【i】，如何记它已经走过了？ 看array [array[i]] already marked negative;
+
 var findDuplicate = function(nums) {
-    let seen = new Set();
-    for (let num of nums) {
-        if (seen.has(num)) {
-            return num;
+    let dup = -1;
+    for (let i = 0; i < nums.length; i++) {
+        
+        let curr = Math.abs(nums[i]);
+        console.log(i, nums[i], nums[curr])
+        if (nums[curr] < 0) {
+            dup = curr;
+            break;
         }
-        seen.add(num);
+        nums[curr] *= -1;
     }
     
-    return -1;
+    return dup;
 };
+
+//time complexity is O(n), space O(1)
