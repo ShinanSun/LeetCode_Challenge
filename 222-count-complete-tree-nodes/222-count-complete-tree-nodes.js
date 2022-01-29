436b15dc285a6b54849274cfbebcdf950ee73a60
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-01-29 12:42:42 -0600

    Time: 139 ms (49.60%), Space: 69.9 MB (5.58%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.js
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.js
@@ -10,54 +10,48 @@
  * @param {TreeNode} root
  * @return {number}
  */
+const findDepth = function(root) {
+    let d = 0;
+    while (root.left) {
+        root = root.left;
+        d++;
+    }
+    return d;
+}
+
+const exist = function(idx, depth, length, root) {
+    let left = 0, right = length - 1;
+    for (let i = 0; i < depth; i++) {
+        let midNode = Math.floor((left + right) / 2);
+        if (idx <= midNode) {
+            root = root.left;
+            right = midNode - 1;
+        } else {
+            root = root.right;
+            left = midNode + 1;
+        }
+    }
+    
+    return root !== null;
+}
 var countNodes = function(root) {
     if (!root) return 0;
+    const d = findDepth(root);
+    if (d === 0) return 1; //when we only have one node, d is 0 indexed;
     
-    const level = findLevel(root);
-    if (!level) return 1;
-   
-    const upperLevelCount = Math.pow(2, level) - 1; 
+    const upperCount = Math.pow(2, d) - 1;
     
-    //find number of nodes in last level;
-    //note that if last level is full, NumberOfNodesInLastLevel= Math.pow(2, level -1);
-    let left = 0, right = Math.pow(2, level) - 1;
+    const lastLevelLength = Math.pow(2, d);
+    let left = 0, right = lastLevelLength - 1;
+    
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-        if (exist(mid, level, root)) {
+        if (exist(mid, d, lastLevelLength, root)) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
     
-    return left + upperLevelCount;
+    return upperCount + left;
 };
-
-// Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
-// Return True if last level node idx exists. 
-const exist = function(idx, level, root) {
-    let left = 0, right = Math.pow(2, level) - 1;
-    let pivot;
-    for (let i = 0; i < level; i++) {
-        pivot = Math.floor((left + right) / 2);
-        if (idx <= pivot) {
-            root = root.left;
-            right = pivot - 1;
-        } else {
-            root = root.right;
-            left = pivot + 1;
-        }
-    }
-    
-    return root !== null;
-}
-
-const findLevel = function(node) {
-    let level = 0;
-    while (node.left) {
-        node = node.left;
-        level++;
-    }
-    
-    return level;
-}
