ea1522d1cf9d047994a0490c05d66e3daaf4180e
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-02-01 15:59:42 -0600

    Time: 839 ms (8.87%), Space: 126.3 MB (5.10%) - LeetHub

--- 146-lru-cache/146-lru-cache.js
+++ 146-lru-cache/146-lru-cache.js
@@ -1,9 +1,46 @@
-/**
- * @param {number} capacity
- */
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.val = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DLL {
+    constructor() {
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.size = 0;
+    }
+    
+    addNode(node) {
+        //always add the new node right before the pseudo tail;
+        node.next = this.tail;
+        node.prev = this.tail.prev;
+        
+        this.tail.prev.next = node;
+        this.tail.prev = node;
+        this.size++;
+    }
+    
+    removeNode(node) {
+        let prev = node.prev;
+        let next = node.next;
+       
+        prev.next = next;
+        next.prev = prev;
+        this.size--;
+    }
+    
+}
+
 var LRUCache = function(capacity) {
     this.capacity = capacity;
-    this.cache = new Map();
+    this.DLL = new DLL();
+    this.map = {};
 };
 
 /** 
@@ -11,12 +48,13 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if (this.cache.has(key)) {
-        let value = this.cache.get(key);
-        this.cache.delete(key);
-        this.cache.set(key, value)
-        return value;
-    };
+    if (this.map[key]) {
+        let node = this.map[key];
+        
+        this.DLL.removeNode(node);
+        this.DLL.addNode(node);
+        return node.val;
+    }
     
     return -1;
 };
@@ -27,12 +65,21 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    this.cache.delete(key);
-    this.cache.set(key, value);
+    let newNode = new Node(key, value);
     
-    if (this.cache.size > this.capacity) {
-        let firstKey = this.cache.keys().next().value;
-        this.cache.delete(firstKey);
+    if (this.map[key]) {
+        let oldNode = this.map[key];
+        this.DLL.removeNode(oldNode);  
+    }
+    
+    this.DLL.addNode(newNode);
+    this.map[key] = newNode;
+    
+
+    if (this.DLL.size > this.capacity) {
+        let removeNode = this.DLL.head.next;
+        this.DLL.removeNode(removeNode);
+        delete this.map[removeNode.key]
     }
 };
 
