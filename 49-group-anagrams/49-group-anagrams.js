e15cc66bec5c3de2ae8cd89eb2805e9a0850667c
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-02-05 12:15:47 -0600

    Time: 250 ms (24.43%), Space: 53.2 MB (16.01%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -1,24 +1,20 @@
 /**
  * @param {string[]} strs
  * @return {string[][]}
- for each str;
-    str.sort and assign it to the map
  */
 var groupAnagrams = function(strs) {
+    if (!strs.length) return [];
+    
     const map = {};
     for (let str of strs) {
-        let sorted = str.split('').sort().join('')
-        if (map[sorted]) {
-            map[sorted].push(str);
-        } else {
-            map[sorted] = [str];
+        let count = new Array(26).fill(0);
+        for (char of str) {
+            let charIndex = char.charCodeAt() - 97;
+            count[charIndex]++;
         }
+        let key = count.join('#');
+        map[key] ? map[key].push(str) : map[key] = [str];
     }
-    
-    let res = [];
-    for (let key in map) {
-        res.push(map[key])
-    }
-    
-    return res;
+   
+    return Object.values(map)
 };
