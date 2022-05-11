b7c5bd43a5290780dd6f0ae7829d8c39eefba097
Shinan Sun <71101247+ShinanSun@users.noreply.github.com>
2022-05-11 17:01:06 -0500

    Time: 139 ms (53.75%), Space: 47.3 MB (65.72%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -14,8 +14,10 @@ var addTwoNumbers = function(l1, l2) {
     let retList = new ListNode();
     let temp = retList;
     let tenth = 0;
-    while(l1 && l2) {
-        let value = l1.val + l2.val + tenth;
+    while(l1 || l2) {
+        let x = l1 ? l1.val : 0;
+        let y = l2 ? l2.val : 0;
+        let value = x + y + tenth;
         if (value >= 10) {
             temp.next = new ListNode(value - 10);
             tenth = 1;
@@ -25,25 +27,11 @@ var addTwoNumbers = function(l1, l2) {
         }
         
         temp = temp.next;
-        l1 = l1.next;
-        l2 = l2.next;
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
     }
     
-  // console.log(l1, l2, retList.next)
-   let rest = l1 || l2;
-   while (rest) {
-       let value = tenth + rest.val;
-       if (value === 10) {
-           temp.next = new ListNode(0);
-           tenth = 1;
-       } else {
-           temp.next = new ListNode(value);
-           tenth = 0;
-       }
-       
-       temp = temp.next;
-       rest = rest.next;
-   }
+
     if (tenth > 0) {
         temp.next = new ListNode(1);
     }
